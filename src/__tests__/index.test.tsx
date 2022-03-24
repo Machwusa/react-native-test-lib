5d8185c2892caae721ed6448344cde562ea76365
Machwusa <b.machwusa@gmail.com>
2022-03-24 11:24:54 +0200

    Update test

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,1 +1,7 @@
-it.todo('write a test');
+import { multiply } from 'react-native-test-lib';
+
+const result = await multiply(3, 7, 2);
+
+test('Test multiply function', () => {
+  expect(result).toBe(42);
+});
